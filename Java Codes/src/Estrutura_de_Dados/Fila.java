e570bdbab360f0c1253c01cc7ba8ec6c7ffeacd9
jacob <rafael.jacob301@gmail.com>
2020-03-01 16:35:28 -0300

    Teste Fila

---
+++ Java Codes/src/Estrutura_de_Dados/Fila.java
@@ -0,0 +1,32 @@
+package Estrutura_de_Dados;
+
+public class Fila {
+	private int[] valores;
+	private int   primeiro;
+	private int   ultimo;
+	private int   total;
+
+	public Fila(){
+		valores = new int[5];
+		primeiro = 0;
+		ultimo = 0;
+		total = 0;
+	}
+
+	public void inserir(){
+		
+	}
+
+	public int retirar(){
+		return
+	}
+
+	public boolean isEmpty() {
+		return
+	}
+
+	public boolean isFull() {
+		return 
+	}
+}
+
